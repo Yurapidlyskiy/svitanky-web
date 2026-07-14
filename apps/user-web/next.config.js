3100b8a91deaa9f918381d9dc9e70bd0dc804552
Yurii Pidlyskyi <yurii.pidlyskyi@intellias.com>
2026-07-14 18:45:20 +0300

    initial setup

--- apps/user-web/next.config.js
+++ apps/user-web/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    transpilePackages: ["@project/common-ui"],
+  };
+  
+  export default nextConfig;
